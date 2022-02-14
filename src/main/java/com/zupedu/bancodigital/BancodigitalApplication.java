6962553690376af33e923a9f65b26bc5563ff50e
paula.santana <paula.santana@zup.com.br>
2022-02-14 10:51:04 -0300

    estrutura do projeto

---
+++ src/main/java/com/zupedu/bancodigital/BancodigitalApplication.java
@@ -0,0 +1,13 @@
+package com.zupedu.bancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zupedu/bancodigital/BancodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupedu.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
