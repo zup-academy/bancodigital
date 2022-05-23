a727ea49afaf5ffccb53a57bfbab4ebfe375abc7
paula.santana <paula.santana@zup.com.br>
2022-05-23 14:14:38 -0300

    Adicionando configurações de monitoramento

--- src/main/java/com/zupedu/bancodigital/transferencia/TransferenciaController.java
+++ src/main/java/com/zupedu/bancodigital/transferencia/TransferenciaController.java
@@ -4,7 +4,6 @@ import com.zupedu.bancodigital.conta.ContaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,13 +30,6 @@ public class TransferenciaController {
         transferenciaRepository.save(transferencia);
 
         return TransferenciaResponse.from(transferencia);
-    }
-
-    @PostMapping("/pix")
-    public ResponseEntity<?> pix(){
-        logger.info("Realizando um pix");
-
-        return ResponseEntity.ok().body("Pix com sucesso");
     }
 
 }
